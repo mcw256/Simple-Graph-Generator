45370d3f0f8e6d5bc914c5e76fbd6eb99d2c189f
mcw256 <rivael256@gmail.com>
2019-11-30 19:38:19 +0100

    moved scene do different file

---
+++ SimpleGraphGenerator/SimpleGraphGenerator/Views/Scene.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SimpleGraphGenerator.Views
+{
+    class Scene : Canvas 
+    {
+        public Scene()
+        {
+            this.Width = 750;
+            this.Height = 750;
+           // this.Background = new "#ecf4ef";
+           // var xd = new Color();
+           // xd.
+           // new SolidColorBrush(Color.)
+
+        }
+
+    }
+}
