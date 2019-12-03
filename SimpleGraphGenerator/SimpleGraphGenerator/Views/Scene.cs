0a62edd6780fe97395d592501f890c8fde80b58a
mcw256 <rivael256@gmail.com>
2019-12-03 21:47:21 +0100

    added dragging handlers

--- SimpleGraphGenerator/SimpleGraphGenerator/Views/Scene.cs
+++ SimpleGraphGenerator/SimpleGraphGenerator/Views/Scene.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -37,9 +38,19 @@ namespace SimpleGraphGenerator.Views
             var settingElls = Creators.CreateSettingElls();
             var edges = Creators.CreateEdgesFromAdjMatrix(adjMatrix);
 
+
             ShapePositioners.ArrangeSettingElls(settingElls);
             ShapePositioners.ArrangeVertexesRandomly(vertexes, settingElls);
             ShapePositioners.ArrangeEdges(edges, vertexes);
+            ShapePositioners.LinkEdgesToVertexes(vertexes, edges);
+
+            foreach (var item in vertexes)
+            {
+                item.MouseLeftButtonDown += VertexMouseLeftButtonDown;
+                item.MouseLeftButtonUp += VertexMouseLeftButtonUp;
+                item.MouseMove += VertexMouseMove;
+            }
+            
 
 
             for (int i = 0; i < settingElls.Count; i++)
@@ -57,7 +68,51 @@ namespace SimpleGraphGenerator.Views
             }       
         }
 
-        
 
+
+
+        bool _isVertexDragInProg;
+        private void VertexMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isVertexDragInProg = true;
+            ((Label)sender).CaptureMouse();
+        }
+
+        private void VertexMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isVertexDragInProg = false;
+            ((Label)sender).ReleaseMouseCapture();
+        }
+
+        private void VertexMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isVertexDragInProg) return;
+
+            // get the position of the mouse relative to the Canvas
+            var mousePos = e.GetPosition(this);
+
+            // center the rect on the mouse
+            double left = mousePos.X - (((Label)sender).ActualWidth / 2);
+            double top = mousePos.Y - (((Label)sender).ActualHeight / 2);
+            Canvas.SetLeft(((Label)sender), left);
+            Canvas.SetTop(((Label)sender), top);
+
+            foreach (var item in ((Vertex)sender).LinkedEdges)
+            {
+                if (item.isBegining)
+                {
+                    item.Line.X1 = left + 13;
+                    item.Line.Y1 = top + 13;
+                }
+                else
+                {
+                    item.Line.X2 = left + 13;
+                    item.Line.Y2 = top + 13;
+                }
+            }
+
+        }
+
+       
     }
 }
