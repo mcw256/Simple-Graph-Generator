d781bde4a5f38aa5b8d22c705c2a3b0454c8c081
mcw256 <rivael256@gmail.com>
2019-12-03 21:45:53 +0100

    added Vertex class

---
+++ SimpleGraphGenerator/SimpleGraphGenerator/Views/SceneUtils/Vertex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace SimpleGraphGenerator.Views.SceneUtils
+{
+    class Vertex : Label 
+    {
+        public List<LineWithInfo> LinkedEdges { get; set; } = new List<LineWithInfo>();
+
+        
+
+    }
+
+    class LineWithInfo
+    {
+        public LineWithInfo(Line line, bool isBegining)
+        {
+            Line = line ?? throw new ArgumentNullException(nameof(line));
+            this.isBegining = isBegining;
+        }
+
+        public Line Line { get; set; }
+        public bool isBegining { get; set; }
+
+    }
+}
